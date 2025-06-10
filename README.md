435b9d010dc394b4a7969324fc3b8504c66d5580
Sebastijan K. <58827427+sebastijankuzner@users.noreply.github.com>
2025-06-10 11:21:20 +0200

    feat: add initial code (#1)

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# bls-public-key-registration-cli
-Simple and lightweight CLI for BLS public key registration on ARK (Mainsail)
+# BLS Public Key Registration CLI
+
+![Logo](logo.png)
+
+Simple and lightweight CLI for BLS public key registration in Core v3.
+
+## Setup
+
+### Install packages:
+
+```bash
+yarn setup
+```
+
+### Config
+
+Edit configuration file `config.json`. Set BLS public key and validators passphrase.
+
+### Send transaction
+
+```bash
+yarn start
+```
+
+## License
+
+[GNU General Public License v3.0](LICENSE) © [ARK Ecosystem](http://arkscic.com/)
---
+++ index.js
@@ -0,0 +1,91 @@
+const { Transactions, Identities, Managers } = require("@arkecosystem/crypto");
+const {
+  newBlsPublicKey,
+  oldBlsPublicKey,
+  passphrase,
+  peer,
+  network,
+} = require("./config.json");
+const { httpie } = require("@arkecosystem/core-utils");
+
+const retrieveSenderNonce = async () => {
+  const address = Identities.Address.fromPassphrase(passphrase);
+  const publicKey = Identities.PublicKey.fromPassphrase(passphrase);
+
+  console.log(
+    `Retrieving nonce for Address ${address}, Public Key: ${publicKey}`
+  );
+
+  try {
+    const response = await httpie.get(`${peer}/api/wallets/${address}`);
+    return Number(response.body.data.nonce);
+  } catch (err) {
+    console.error(
+      `Cannot retrieve nonce for address ${address}: ${err.message}`
+    );
+    return 0;
+  }
+};
+
+const postTransaction = async (tx) => {
+  try {
+    const response = await httpie.post(`${peer}/api/transactions`, {
+      headers: { "Content-Type": "application/json", port: 4003 },
+      body: {
+        transactions: [tx.toJson()],
+      },
+    });
+
+    if (response.status !== 200 || response.body.errors) {
+      console.log("Fail: ", JSON.stringify(response.body));
+
+      return response.body;
+    } else {
+      console.log("Success: ", JSON.stringify(response.body));
+
+      return response.body;
+    }
+  } catch (ex) {
+    console.log("Exception: ", JSON.stringify(ex.message));
+  }
+};
+
+const buildTransaction = async (nonce) => {
+  const builder = Transactions.BuilderFactory.blsPublicKeyRegistration();
+
+  const tx = await builder
+    .version(2)
+    .nonce(nonce)
+    .blsPublicKeyAsset({
+      newBlsPublicKey: newBlsPublicKey,
+      oldBlsPublicKey: oldBlsPublicKey.length > 0 ? oldBlsPublicKey : undefined,
+    })
+    .sign(passphrase)
+    .build();
+
+  return tx;
+};
+
+const main = async () => {
+  Managers.configManager.setFromPreset(network);
+
+  console.log("New BLS Public Key: ", newBlsPublicKey);
+  console.log(
+    "Old BLS Public Key: ",
+    oldBlsPublicKey.length > 0 ? oldBlsPublicKey : "None"
+  );
+  console.log("Passphrase: ", passphrase);
+
+  const nonce = await retrieveSenderNonce();
+
+  console.log(`Nonce: ${nonce}`);
+
+  const tx = await buildTransaction(nonce + 1);
+  console.log(tx.toJson());
+
+  await new Promise((resolve) => setTimeout(resolve, 200));
+
+  await postTransaction(tx);
+};
+
+main();
